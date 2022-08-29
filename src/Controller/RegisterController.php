043691ce01fdc45703ba570d328a604ae4128066
irouf92 <109795361+irouf92@users.noreply.github.com>
2022-08-29 17:03:08 +0200

    Create Controller & Create FormType

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/inscription', name: 'user_register', methods: ['GET', 'POST'])]
+    public function register(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $passwordHasher
+    ): Response
+    {
+      
+    }
+}
