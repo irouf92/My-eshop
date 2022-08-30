06c31ac96798991f6500f4f5def728e1fdb080e7
irouf92 <109795361+irouf92@users.noreply.github.com>
2022-08-30 12:31:24 +0200

    Add register action

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Entity\User;
+use App\Form\RegisterFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +18,45 @@ class RegisterController extends AbstractController
     public function register(Request $request, EntityManagerInterface $entityManager,UserPasswordHasherInterface $passwordHasher
     ): Response
     {
+        #instanciation
+      $user = new User();
+
+# 2 - Création du formulaire + mécanisme d'auto-hydratation
+
+      $form = $this->createForm(RegisterFormType::class, $user)->handleRequest($request);
+
+      #Au clic du boutton "validé"
+      if($form->isSubmitted() && $form->isValid()) {
       
+        #Set des proprietes qui ne sont pas dans le formulaire
+        $user->setCreatedAt(new DateTime());
+        $user->setUpdatedAt(new DateTime());
+        #La propriete "role" est un array [tablaeu]
+        $user->setRoles(['ROLE_USER']);
+
+        #Nous devons resetter manuellement le password car par defaut il n est pas haché
+        #Pour cela nous devons utiliser une methode de hachage, appellée hashPassword() :
+            # => cette méthode prend 2 arguments : $user, $plainPassword
+
+        
+        $user->setPassword(
+            $passwordHasher->hashPassword(
+                $user, $form->get('password')->getData())
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('default_home');
+      }//end if
+
+     # 3 - Rendu de la vue Twig, avec le formulaire
+
+      return $this ->render('register/form.html.twig',[
+        'form' => $form->createView()# createView() permet de générer le HTML pour l'affichage
+
+      ]);
+      
+      }
     }
-}
+
